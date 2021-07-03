2546c62b0c71ec096a9d405ad315b545b1200e88
Michael CHEN <78593987+michaelchen753@users.noreply.github.com>
2021-07-03 15:53:43 +1000

    fixed bugs

--- src/components/ListItem/ListItem.js
+++ src/components/ListItem/ListItem.js
@@ -3,9 +3,8 @@ import React from 'react';
 export default function ListItem({todo, deleteTodo, updateTodos}) {
     const {title, completed, id} = todo;
     return (
-        <div>
-           <span>{title}</span>
-           <span className={`${completed? 'green':'red'}`}>completed</span>
+        <div className='list-item'>
+           <span className={`${completed? 'red':'green'}`}>{title}</span>           
            <button type='submit' onClick={()=> updateTodos(id)} >&#10067;</button>
            <button type='submit' onClick={()=> deleteTodo(id)} >&#10006;</button>  
         </div>
