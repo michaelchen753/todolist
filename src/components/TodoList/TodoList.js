ccf87efcfd7f6b3251343d3a56349fd5dff901ff
Michael CHEN <78593987+michaelchen753@users.noreply.github.com>
2021-07-05 06:57:07 +1000

    Revert " fix bugs"

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -4,22 +4,18 @@ import ListItem from '../ListItem/ListItem';
 export default function TodoList({todos, updateTodos, deleteTodo}) {
     return (
         <div className='todo-list'>
-            <ol>
-                {
-                    todos.map((todo,index)=>{
-                        return (
-                            <li>
-                                 <ListItem 
-                                    key={todo.id} 
-                                    todo={todo} 
-                                    updateTodos={updateTodos} 
-                                    deleteTodo={deleteTodo}                            
-                                    />
-                            </li>                           
-                        )
-                    })
-                }
-            </ol>    
+            {
+                todos.map((todo,index)=>{
+                    return (
+                        <ListItem 
+                        key={todo.id} 
+                        todo={todo} 
+                        updateTodos={updateTodos} 
+                        deleteTodo={deleteTodo}                            
+                        />
+                    )
+                })
+            }
         </div>
     )
 };
